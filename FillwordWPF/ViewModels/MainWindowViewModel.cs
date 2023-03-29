255d5343056222c12091265a595923d530bbf9ab
zhora <zoracementov92551@gmail.com>
2023-03-29 17:59:07 +0300

    MainWindowViewModel

---
+++ FillwordWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace FillwordWPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок Окна
+
+        private string _title = "Fillwords";
+
+        /// <summary>
+        /// Заголовок главного окна
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            private set => Set(ref _title, value);
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
---
+++ FillwordWPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FillwordWPF.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+    }
+}
