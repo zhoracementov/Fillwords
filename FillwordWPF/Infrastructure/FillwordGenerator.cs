a987628a93d46ee30af665f3929dfd8c6be7a2b6
zhora <zoracementov92551@gmail.com>
2023-03-30 10:23:20 +0300

    generator recode

--- FillwordWPF/Infrastructure/FillwordGenerator.cs
+++ FillwordWPF/Infrastructure/FillwordGenerator.cs
@@ -1,21 +1,25 @@
-﻿using FillwordWPF.Infrastructure;
+﻿using FillwordWPF.Models;
 using FillwordWPF.Services;
+using System;
+using System.Threading.Tasks;
 
-namespace FillwordWPF.Models
+namespace FillwordWPF.Infrastructure
 {
     internal class FillwordGenerator
     {
         private const string URL = @"https://raw.githubusercontent.com/Harrix/Russian-Nouns/main/src/data.json";
-
-        public Fillword Fillword { get; }
+        public Task<Fillword> LoadingTask { get; }
 
         public FillwordGenerator()
         {
             WordsData wordsData = null;
             var gameSettings = new GameSettings();
             var loader = new JsonWebDataLoader<WordsData>(URL, gameSettings.SaveDataFileName);
-            loader.LoadData().ContinueWith(x => wordsData = loader.Data);
-            Fillword = new FillwordTableRandomBuilder(wordsData, gameSettings.Difficulty).Build();
+
+            LoadingTask = loader
+                .LoadData()
+                .ContinueWith(x => new FillwordTableRandomBuilder(wordsData, gameSettings.Difficulty)
+                .Build());
         }
     }
 }
