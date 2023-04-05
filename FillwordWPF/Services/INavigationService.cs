fa0b7b081f7548e7ebfcea657e429af899eefabd
zhora <zoracementov92551@gmail.com>
2023-04-05 21:03:34 +0300

    NavigationService

---
+++ FillwordWPF/Services/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using FillwordWPF.ViewModels;
+
+namespace FillwordWPF.Services
+{
+    internal interface INavigationService
+    {
+        public ViewModel CurrentViewModel { get; }
+        void NavigateTo<T>() where T : ViewModel;
+    }
+}
---
+++ FillwordWPF/Services/NavigateService.cs
@@ -0,0 +1,32 @@
+﻿using FillwordWPF.ViewModels;
+using System;
+
+namespace FillwordWPF.Services
+{
+    internal class NavigationService : ViewModel, INavigationService
+    {
+        private readonly Func<Type, ViewModel> viewModelFactory;
+        private ViewModel currentViewModel;
+        public ViewModel CurrentViewModel
+        {
+            get => currentViewModel;
+            private set
+            {
+                OnPropertyChanged();
+                currentViewModel = value;
+            }
+        }
+
+        public NavigationService(Func<Type, ViewModel> viewModelFactory)
+        {
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public void NavigateTo<TViewModel>() where TViewModel : ViewModel
+        {
+            CurrentViewModel = viewModelFactory.Invoke(typeof(TViewModel));
+
+            throw new NotImplementedException();
+        }
+    }
+}
